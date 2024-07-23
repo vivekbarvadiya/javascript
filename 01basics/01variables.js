fdc38ee9fe49a286d47b518069d0551c76bd4ad3
vivek barvadiya <96981978+vivekbarvadiya@users.noreply.github.com>
2024-07-23 07:21:16 +0000

    datatypes

---
+++ 01basics/01variables.js
@@ -0,0 +1,12 @@
+accountCity = "jaipur";
+/*
+use let and const but not var because var is block/functional scpope
+*/
+console.log(accountCity);
+const accountId = 133;
+let b = 'vivek';
+var c = 2.4;
+let d;
+var e;
+
+console.table([accountId,b,c,accountCity,d,e]);
---
+++ 01basics/02datatypes.js
@@ -0,0 +1,17 @@
+"Use strict"; //treat all js code as new version
+
+// alert(3+3) // we are using node.js not browser
+
+// number => 2 power 53 range
+// bigint => not preferable
+// string => "",'
+// boolean => t/f
+// null => standalone value
+// undefined => value is not assigned
+// symbol => for uniques 
+// Object => 
+
+console.log(typeof "45"); //string
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
