e7f3ba7437a72ce4ca02059b5be2efa8241c6abb
vivek barvadiya <96981978+vivekbarvadiya@users.noreply.github.com>
2024-07-23 07:42:34 +0000

    conversion of datatypes

--- 01basics/03coversionNOperation.js
+++ 01basics/03coversionNOperation.js
@@ -1,2 +1,22 @@
-let score = 33
-console.log(typeof score);
+let score = false
+
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber);
+// console.log(typeof NaN);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false => 0
+
+let bool = 0;
+let boolInt = Boolean(bool);
+
+console.log(boolInt);
+
+// 1 => t,0 => f
+// "" => f , "any content" => t
+
+// String(number) => number to string conversion
