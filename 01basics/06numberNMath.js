3f5d40ffad6759b4623d1dfdd9a500f80beeff87
vivek barvadiya <96981978+vivekbarvadiya@users.noreply.github.com>
2024-07-24 05:45:41 +0000

    number and math

---
+++ 01basics/06numberNMath.js
@@ -0,0 +1,63 @@
+const score = 400
+// console.log(score);
+// console.log(score + "win");
+
+
+const balance = new Number(300.78954)
+// console.log(balance);
+// console.log(balance + "win");
+
+
+// to access string property in numbers, first convert numbers to string.
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const num = 123.2343
+const num2 = 12.6478
+
+// toPrecision method will round of/ give precise number of given number
+// console.log(num.toPrecision(3));
+// console.log(num2.toPrecision(2));
+
+
+const hundreds = 100000000
+
+// console.log(hundreds.toLocaleString());
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+// ********************MATH**********************
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(1,2,3,4));
+// console.log(Math.max(1,2,3,4));
+
+// console.log(Math.floor(Math.random()*10)+1);
+
+// formula to generate random between range
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
