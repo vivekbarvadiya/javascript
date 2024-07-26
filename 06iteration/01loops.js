0958df0831992034ebcc8d933d6b50a0929029cf
vivek barvadiya <96981978+vivekbarvadiya@users.noreply.github.com>
2024-07-26 17:55:02 +0000

    loops

---
+++ 06iteration/01loops.js
@@ -0,0 +1,70 @@
+// for
+
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);
+    
+// }
+
+// const array = ["a","b","c"]
+
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element);
+    
+// }
+
+
+
+// break and continue
+
+
+// for (let index = 0; index < 20; index++) {
+
+//     if (index==5) {
+//         console.log("detected 5");
+//         break //completely out of the loop
+//     }
+//     console.log(index);
+    
+// }
+
+
+// for (let index = 0; index < 20; index++) {
+
+//     if (index==5) {
+//         console.log("detected 5");
+//         continue //skip the condition and continue next condition
+//     }
+//     console.log(index);
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 06iteration/02dowhile.js
@@ -0,0 +1,35 @@
+// while, do-while
+
+// let index = 0
+// while(index<=10){
+//     console.log(index);
+//     index+=2
+// }
+
+let score = 1
+
+do{
+    console.log(score);
+    score+=1
+}while(score<=10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
